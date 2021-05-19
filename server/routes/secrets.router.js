03ab6c44413ea849cb79f6aef26d046c090085b3
SamMoudry <sammoudry@gmail.com>
2021-05-19 00:23:00 -0500

    authentication completed

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const {
+  rejectUnauthenticated,
+} = require('../modules/authentication-middleware');
 
 router.get('/', (req, res) => {
   // what is the value of req.user????
   console.log('req.user:', req.user);
-
+  rejectUnauthenticated(req, res);
   pool
     .query(`SELECT * FROM "secret";`)
     .then((results) => res.send(results.rows))
